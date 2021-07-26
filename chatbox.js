d322b5949a02d0dd47aa00ca88f02641f03ed05e
cafemmo <87969271+cafemmo@users.noreply.github.com>
2021-07-26 16:29:21 +0700

    Update chatbox.js

--- chatbox.js
+++ chatbox.js
@@ -1,7 +1,7 @@
     document.write(`<link rel="stylesheet" type="text/css" href="https://www.shoutbox.com/chat/css/shoutbox.css?cache=1626884780">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sprintf/1.1.2/sprintf.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
         <script src="https://www.shoutbox.com/js/socket.io.js"></script>
         <div class="shoutBoxContainer">
             <div id="shoutBoxHeader" class="shoutBoxHeader">
